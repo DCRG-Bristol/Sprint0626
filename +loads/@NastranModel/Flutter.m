1f37c0d4ae909cbe822afc5764ea8a39e01c999c
SanujaJ <90722949+SanujaJ@users.noreply.github.com>
2025-06-02 21:34:21 +0100

    adjusted SetConfigur.. in loads/Nastran to handle mass changes

--- +loads/@NastranModel/Flutter.m
+++ +loads/@NastranModel/Flutter.m
@@ -6,7 +6,7 @@ arguments
     opts.IsLocked = true;
     opts.EV = false;
     opts.Alts = 60e3:-1e3:0;
-    opts.Rebuild = false;
+    opts.Rebuild = true;
 end
 obj.SetConfiguration(FuelMass=obj.Taw.MTOM*obj.Taw.Mf_Fuel*obj.Taw.Mf_TOC,IsLocked=opts.IsLocked, Rebuild=opts.Rebuild)
 [BinFolder,flut_res] = obj.ISO145(Mach,TruelySilent=~opts.Verbose,GetEigenVectors=opts.EV,Alts=opts.Alts);
