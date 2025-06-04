31ea7f25a29a08c732f19fe800fb69f041410a8e
Fintan Healy <fintan.healy@bristol.ac.uk>
2025-06-04 22:58:36 +0100

    fixed Nuhaadh's gift

--- @TAW/GetHingeFairingSurrogate.m
+++ @TAW/GetHingeFairingSurrogate.m
@@ -2,48 +2,12 @@ function [K_fair,M_fair] = GetHingeFairingSurrogate(ADP)
 %GETHINGEFAIRINGSURROGATE Summary of this function goes here
 % Calculates the added torsional stiffness and mass by the hinge fairing 
 
-% Variables
-AR = ADP.AR;
-SweepAngle = ADP.SweepAngle;
-HingeEta = ADP.HingeEta;
-FlareAngle = ADP.FlareAngle;
-M_c = ADP.ADR.M_c;
-
-
-%% Checking for ranges
-% Values
-values = {
-    'AR', AR;
-    'SweepAngle', SweepAngle;
-    'HingeEta', HingeEta;
-    'FlareAngle', FlareAngle;
-    'M_c', M_c
-};
-
-% Ranges
-ranges = {
-    'AR', [12, 22];
-    'SweepAngle', [0, 40];
-    'HingeEta', [0.5, 0.99];
-    'FlareAngle', [5, 35];
-    'M_c', [0.5, 0.85]
-};
-
-% Check ranges and raise error if out of range
-for i = 1:size(ranges, 1)
-    param = ranges{i, 1};
-    range = ranges{i, 2};
-    value = values{i, 2};
-
-    if isempty(value)
-        error('Value for %s is empty', param);
-    end
-   
-    if value < range(1) || value > range(2)
-        error('Value for %s is out of range: %f (expected between %f and %f)', param, value, range(1), range(2));
-    end
-end
-
+% Variables (bounded to interpolation range)
+AR = min(max(ADP.AR,12),22);
+SweepAngle = min(max(ADP.SweepAngle,0),40);
+HingeEta = min(max(ADP.HingeEta,0.5),0.99);
+FlareAngle = min(max(ADP.FlareAngle,5),35);
+M_c = min(max(ADP.ADR.M_c,0.5),0.85);
 
 %% interpolation
 
@@ -73,7 +37,7 @@ for i = 1:numel(V6)
 end
 
 % Interpolation
-if ~obj.EnableFairingStiffness
+if ~ADP.EnableFairingStiffness
     K_fair = 0;
 else
     K_fair = interpn(V1,V2,V3,V4,V5,V6,AR,SweepAngle,HingeEta,FlareAngle, M_c); 
