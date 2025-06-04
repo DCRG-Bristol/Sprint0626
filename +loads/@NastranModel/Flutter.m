1b2c9e6cfe13090f7653f3f6167dff84d3f44bf3
Fintan Healy <fintan.healy@bristol.ac.uk>
2025-06-04 17:52:20 +0100

    Dynamics Team First Surrogate

--- +loads/@NastranModel/Flutter.m
+++ +loads/@NastranModel/Flutter.m
@@ -6,7 +6,8 @@ arguments
     opts.IsLocked = true;
     opts.EV = false;
     opts.Alts = 60e3:-1e3:0;
+    opts.Rebuild = true;
 end
-obj.SetConfiguration(FuelMass=obj.Taw.MTOM*obj.Taw.Mf_Fuel*obj.Taw.Mf_TOC,IsLocked=opts.IsLocked)
+obj.SetConfiguration(FuelMass=obj.Taw.MTOM*obj.Taw.Mf_Fuel*obj.Taw.Mf_TOC,IsLocked=opts.IsLocked, Rebuild=opts.Rebuild)
 [BinFolder,flut_res] = obj.ISO145(Mach,TruelySilent=~opts.Verbose,GetEigenVectors=opts.EV,Alts=opts.Alts);
 end
