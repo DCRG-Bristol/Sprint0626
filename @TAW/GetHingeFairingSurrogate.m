354f9dec0d7fb3b16359f45986ebe27f03858a42
fh9g12 <qe19391@bristol.ac.uk>
2025-06-04 11:41:27 +0100

    WIP - Fairing Surrogate Template

---
+++ @TAW/GetHingeFairingSurrogate.m
@@ -0,0 +1,9 @@
+function [K_fair,M_fair] = GetHingeFairingSurrogate(ADP)
+%GETHINGEFAIRINGSURROGATE Summary of this function goes here
+%   Detailed explanation goes here
+
+% load(fullfile(fileparts(mfilename('fullpath')),'private','LIFT_DISTRIBUTION_V1.mat'),'lift_dist');
+   
+K_fair = 0;
+M_fair = 0;
+end
