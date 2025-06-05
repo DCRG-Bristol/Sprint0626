d8c4f9b59d62eb32142e52a6626ed2d3f752c0bc
Sander van den Broek <sander.vandenbroek@bristol.ac.uk>
2025-06-05 09:55:11 +0100

    Fixed call to the wrong parameter

--- sizeSample.m
+++ sizeSample.m
@@ -80,7 +80,8 @@ if printoutput
     %% ======================== Get Mission Fuel Burn =========================
     
     fh.printing.title('','Length',60,'Symbol','=')
-    fh.printing.title(sprintf('Trip Fuel: %.3f t',meta.Fuel_trip./1e3),'Length',60,'Symbol','=')
+    fh.printing.title(sprintf('Trip Fuel: %.3f t',Fuel_trip./1e3),'Length',60,'Symbol','=')
+    fh.printing.title(sprintf('Block Fuel: %.3f t',block_trip./1e3),'Length',60,'Symbol','=')
     fh.printing.title(sprintf('MTOM: %.2f t',ADP.MTOM),'Length',60,'Symbol','=')
 
     %% ======================== Get Operating Cost  ===========================
